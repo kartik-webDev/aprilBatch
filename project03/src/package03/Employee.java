25e9853f50ab3df7301258450eded57d43368774
ASUS <ASUS@Skywalker.mshome.net>
2025-04-19 17:50:53 +0530

    After Changes

--- project03/src/package03/Employee.java
+++ project03/src/package03/Employee.java
@@ -1,9 +1,34 @@
 package package03;
 
 public class Employee {
-
+	
+	String EmpName = "Rohan";
+	
+	String EmpProfesssion = "Backend Dev";
+	
+	int EmpSalary = 250000;
+	
+	String EmpRegion = "Asia";
+	
+	
 	public static void main(String[] args) {
-		System.out.println("hello employee");
+		System.out.println("this is main method");
+		
+		Employee empObj = new Employee();
+		
+		System.out.println(empObj.EmpName);
+		
+		System.out.println(empObj.EmpProfesssion);
+		
+		System.out.println(empObj.EmpSalary);
+		
+		System.out.println(empObj.EmpRegion);
+
+		
+		
+
+		
+		
 	}
 
 }
