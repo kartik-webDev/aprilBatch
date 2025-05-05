361ca69ceb0c52aadb88cc9276743512a84b26be
kartik-webDev <kartik.microservices.dev@gmail.com>
2025-05-05 22:39:04 +0530

    new changes

--- project03/src/package04/Coaching.java
+++ project03/src/package04/Coaching.java
@@ -1,9 +1,26 @@
 package package04;
 
+
 public class Coaching {
+	
+	public Coaching()
+	{
+		this("h1", "h2");
+		System.out.println("this is default constructor");
+	}
+	public Coaching(String a)
+	{
+		System.out.println("this is one parameterized constructor");
+	}
+	public Coaching(String a, String b)
+	{
+		this("h3");
+		System.out.println("this is two parameterized constructor");
+	}
 
 	public static void main(String[] args) {
-		System.out.println("this is may batch");
+		Coaching obj = new Coaching();
+		System.out.println("hello new changes");
 	}
 
 }
